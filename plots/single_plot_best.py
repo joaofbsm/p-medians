5744b6e92924ac26a4dd0ef203ebde697aa4ca4e
Joao Martins <joaofbsm@hotmail.com>
2017-11-07 15:29:59 -0200

    Add documentation, Update plot scripts

--- plots/single_plot_best.py
+++ plots/single_plot_best.py
@@ -18,7 +18,7 @@ n_repetitions = 30
 datasets = [("SJC1", 100, 10), ("SJC2", 200, 15), ("SJC3b", 300, 30)]
 
 default_iterations = 50
-n_iterations = [25, 50, 100, 500]
+n_iterations = [25, 50, 100]
 default_rho = 0.9
 rhos = [0.0, 0.1, 0.5, 0.99]
 default_alpha = 0.5
@@ -37,6 +37,8 @@ for dataset_name, n, p in datasets:
     datasets_best = {"SJC1": (100, 2 * (n - p), 0.99, 3.0, 1.0), 
                      "SJC2": (100, 2 * (n - p), 0.99, 1.0, 0.0), 
                      "SJC3b": (100, 2 * (n - p), 0.9, 3.0, 1.0)}
+
+    dataset_best_distance = np.inf
 
     # Iteration plots
     color_index = 0
@@ -57,6 +59,9 @@ for dataset_name, n, p in datasets:
 
         g_best = g_best[:, 1:]
         best = np.amin(g_best, axis=1)
+
+        if best[-1] < dataset_best_distance:
+            dataset_best_distance = best[-1]
 
         label = "{} iterations | {} ants | rho {} | alpha {}, beta {}".format(iterations, default_ants, default_rho, default_alpha, default_beta)
         ax.plot(np.arange(iterations), best, label=label, color=getColor("viridis", len(n_iterations), color_index))
@@ -82,6 +87,9 @@ for dataset_name, n, p in datasets:
         g_best = g_best[:, 1:]
         best = np.amin(g_best, axis=1)
 
+        if best[-1] < dataset_best_distance:
+            dataset_best_distance = best[-1]
+
         label = "{} iterations | {} ants | rho {} | alpha {}, beta {}".format(default_iterations, ants, default_rho, default_alpha, default_beta)
         ax.plot(np.arange(default_iterations), best, label=label, color=getColor("magma", len(n_ants), color_index))
         color_index += 1
@@ -104,6 +112,9 @@ for dataset_name, n, p in datasets:
 
         g_best = g_best[:, 1:]
         best = np.amin(g_best, axis=1)
+
+        if best[-1] < dataset_best_distance:
+            dataset_best_distance = best[-1]
 
         label = "{} iterations | {} ants | rho {} | alpha {}, beta {}".format(default_iterations, default_ants, rho, default_alpha, default_beta)
         ax.plot(np.arange(default_iterations), best, label=label)
@@ -130,12 +141,15 @@ for dataset_name, n, p in datasets:
         g_best = g_best[:, 1:]
         best = np.amin(g_best, axis=1)
 
+        if best[-1] < dataset_best_distance:
+            dataset_best_distance = best[-1]
+
         label = "{} iterations | {} ants | rho {} | alpha {}, beta {}".format(default_iterations, default_ants, default_rho, alpha, beta)
         ax.plot(np.arange(default_iterations), best, label=label)
 
     # Best of all
-    iterations, ants, rho, alpha, beta = datasets_best["dataset_name"]
-    g_best = np.zeros((default_iterations, 1))
+    iterations, ants, rho, alpha, beta = datasets_best[dataset_name]
+    g_best = np.zeros((iterations, 1))
     for repetition in range(n_repetitions):
         file_path = "../results/{}it{}rho{}alpha{}beta{}ants{}/{}.csv".format(
                                                                   dataset_name, 
@@ -147,15 +161,25 @@ for dataset_name, n, p in datasets:
                                                                   repetition)
 
         content = np.genfromtxt(file_path,  dtype = "float", delimiter = ",")
-        g_best = np.hstack((g_best, content[:, 1].reshape((default_iterations, 1))))
+        g_best = np.hstack((g_best, content[:, 1].reshape((iterations, 1))))
 
     g_best = g_best[:, 1:]
     best = np.amin(g_best, axis=1)
 
-    label = "BEST - {} iterations | {} ants | rho {} | alpha {}, beta {}".format(iterations, ants, rho, alpha, beta)
-    ax.plot(np.arange(iterations), best, label=label)
+    if best[-1] < dataset_best_distance:
+        dataset_best_distance = best[-1]   
 
-    legend = ax.legend(loc='upper right')
+    label = "BEST - {} iterations | {} ants | rho {} | alpha {}, beta {}".format(iterations, ants, rho, alpha, beta)
+    ax.plot(np.arange(iterations), best, label=label, color='m')
+
+    print("Best value for {}: {}".format(dataset_name, dataset_best_distance))
+
+    if dataset_name == "SJC1":
+        legend = ax.legend(loc='center right')
+    elif dataset_name == "SJC3b":
+        legend = ax.legend(loc='center left')
+    else:
+        legend = ax.legend(loc='upper right')
 
     #plt.show(block=False)
     #input("Hit Enter To Close")
